4108af91422cb4a11b6def7e2072d311975197a0
Joyce Wang <75396791+joyceyiyiwang@users.noreply.github.com>
2025-10-08 16:35:59 -0500

    Update 05c_edit_merged_fam.R

--- 05c_edit_merged_fam.R
+++ 05c_edit_merged_fam.R
@@ -8,11 +8,9 @@ fam_file <- read_delim('/scratch/06568/joyce_w/pgs_portability_questions/data/uk
 nwb <- read_delim('data/ukb_populations/nwb_all_id.txt', delim = ' ', trim_ws = T)
 
 wb_gwas <- read_delim('data/ukb_populations/wb_gwas_id.txt', delim = ' ', trim_ws = T)
-wb_pred <- read_delim('data/ukb_populations/wb_pred_id.txt', delim = ' ', trim_ws = T)
 
 combined_labels <- bind_rows(
   wb_gwas %>% mutate(pop = 'GWAS'),
-  wb_pred %>% mutate(pop = str_glue('pred{IID}') %>% as.character),
   nwb %>% mutate(pop = str_glue('pred{IID}') %>% as.character)
 )
 
