9bf33c96e3b1da259c2efe8b3e880ec4be8b5736
Joyce Wang <75396791+joyceyiyiwang@users.noreply.github.com>
2025-12-13 02:11:32 -0600

    Update 15e_ind_level_pred_sensitivity.R

--- 15e_ind_level_pred_sensitivity.R
+++ 15e_ind_level_pred_sensitivity.R
@@ -613,3 +613,307 @@ pgs_df_16$pred_error = (pgs_df_16$Z - pgs_df_16$Z_hat) ^ 2
 pgs_df_16 <- pgs_df_16 %>% arrange(IID, phenotype)
 
 pgs_df_16 %>% write_tsv("data/pgs_pred/ind_pgs_df_16.tsv")
+
+
+# Townsend index w/ 5 strata
+group_non_pgs_df_townsend_5 <- read_tsv("data/pgs_pred/group_non_pgs_df_townsend_5_strata.tsv")
+
+# Read in PGS for threshold 1 (p < 1e-5)
+pgs_df_townsend_5 <- data.frame()
+for(trait in pheno){
+  temp <- read_tsv(paste0("data/pgs/", trait, "_1_scores.sscore"))
+  temp$pgs <- temp$SCORE1_AVG
+  temp <- temp %>% select(-c(ALLELE_CT, NAMED_ALLELE_DOSAGE_SUM, SCORE1_AVG))
+  temp$phenotype <- rep(trait, nrow(temp))
+  pgs_df_townsend_5 <- rbind.data.frame(pgs_df_townsend_5, temp)
+  rm(temp)
+}
+
+pgs_df_townsend_5 <- left_join(group_non_pgs_df_townsend_5, pgs_df_townsend_5, by = c("#FID", "IID", "phenotype"))
+pgs_df_townsend_5 <- pgs_df_townsend_5 %>% select(`#FID`, IID, sex, age, age_sq, age_sex, age_sq_sex, array_type,
+                            pc_dist, pgs, phenotype, phenotype_value, weighted_pc_groups, group_close_to_gwas)
+
+pgs_df_townsend_5 <- pgs_df_townsend_5 %>% mutate(
+  sex_pc_dist = sex * pc_dist,
+  pc_dist_sq = pc_dist ^ 2,
+  pc_dist_3 = pc_dist ^ 3,
+  pc_dist_4 = pc_dist ^ 4,
+  pc_dist_5 = pc_dist ^ 5,
+  pc_dist_6 = pc_dist ^ 6,
+  pc_dist_7 = pc_dist ^ 7,
+  pc_dist_8 = pc_dist ^ 8,
+  pc_dist_9 = pc_dist ^ 9,
+  pc_dist_10 = pc_dist ^ 10,
+  pc_dist_11 = pc_dist ^ 11,
+  pc_dist_12 = pc_dist ^ 12,
+  pc_dist_13 = pc_dist ^ 13,
+  pc_dist_14 = pc_dist ^ 14,
+  pc_dist_15 = pc_dist ^ 15,
+  pc_dist_16 = pc_dist ^ 16,
+  pc_dist_17 = pc_dist ^ 17,
+  pc_dist_18 = pc_dist ^ 18,
+  pc_dist_19 = pc_dist ^ 19,
+  pc_dist_20 = pc_dist ^ 20
+)
+
+# Fit a linear model of Y ~ covariates in each bin independently, 
+# startng with the first bin
+temp <- pgs_df_townsend_5 %>% filter(phenotype == pheno[1] & weighted_pc_groups == 1)
+# Only use array_type if both array types have more than 1 sample in this bin
+# (or lm doesn't run)
+if(length(temp$array_type[temp$array_type == "Axiom"]) > 1 &
+   length(temp$array_type[temp$array_type == "BiLEVE"]) > 1){
+  Y_mod <- temp %>%
+    lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex + array_type, data = .)
+  
+  # Apply the model onto this weighted_pc_groups
+  temp$Y_hat <- unname(predict(Y_mod, newdata = temp))
+  
+} else{
+  Y_mod <- temp %>%
+    lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex, data = .)
+  
+  # Apply the model onto this weighted_pc_groups
+  temp$Y_hat <- unname(predict(Y_mod, newdata = temp))
+}
+
+# Repeat for the other bins
+for(i in 2:250){
+  temp2 <- pgs_df_townsend_5 %>% filter(phenotype == pheno[1] & weighted_pc_groups == i)
+  if(length(temp2$array_type[temp2$array_type == "Axiom"]) > 1 &
+     length(temp2$array_type[temp2$array_type == "BiLEVE"]) > 1){
+    Y_mod <- temp2 %>%
+      lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex + array_type, data = .)
+    
+    temp2$Y_hat = unname(predict(Y_mod, newdata = temp2))
+    
+  } else{
+    Y_mod <- temp2 %>%
+      lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex, data = .)
+    
+    temp2$Y_hat <- unname(predict(Y_mod, newdata = temp2))
+  }
+  temp <- rbind.data.frame(temp, temp2)
+}
+
+# Do the same for the other phenotypes
+for(trait in pheno[2:15]){
+  print(trait)
+  for(i in 1:250){
+    temp2 <- pgs_df_townsend_5 %>% filter(phenotype == trait & weighted_pc_groups == i)
+    if(length(temp2$array_type[temp2$array_type == "Axiom"]) > 1 &
+       length(temp2$array_type[temp2$array_type == "BiLEVE"]) > 1){
+      Y_mod <- temp2 %>%
+        lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex + array_type, data = .)
+      
+      temp2$Y_hat = unname(predict(Y_mod, newdata = temp2))
+      
+    } else{
+      Y_mod <- temp2 %>%
+        lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex, data = .)
+      
+      temp2$Y_hat <- unname(predict(Y_mod, newdata = temp2))
+    }
+    temp <- rbind.data.frame(temp, temp2)
+  }
+}
+
+pgs_df_townsend_5 <- temp %>% arrange(IID)
+
+# X = Y - Y_hat
+pgs_df_townsend_5$X <- pgs_df_townsend_5$phenotype_value - pgs_df_townsend_5$Y_hat
+
+# Do X ~ gen_dist_polynomial + sex + sex_gen_dist across all bins
+X_mod <- pgs_df_townsend_5 %>%
+  group_by(phenotype) %>%
+  do(lm_X = lm(X ~ pc_dist + pc_dist_sq + pc_dist_3 + pc_dist_4 + pc_dist_5 + pc_dist_6 +
+                 pc_dist_7 + pc_dist_8 + pc_dist_9 + pc_dist_10 + pc_dist_11 + pc_dist_12 +
+                 pc_dist_13 + pc_dist_14 + pc_dist_15 + pc_dist_16 + pc_dist_17 + pc_dist_18 +
+                 pc_dist_19 + pc_dist_20 + sex + sex_pc_dist, data = .))
+
+# Apply this model
+temp <- pgs_df_townsend_5 %>% filter(phenotype == pheno[1])
+temp$X_hat <- unname(predict(X_mod$lm_X[X_mod$phenotype == pheno[1]][[1]], newdata = temp))
+for(trait in pheno[2:15]){
+  temp2 <- pgs_df_townsend_5 %>% filter(phenotype == trait)
+  temp2$X_hat <- unname(predict(X_mod$lm_X[X_mod$phenotype == trait][[1]], newdata = temp2))
+  temp <- rbind.data.frame(temp, temp2)
+}
+pgs_df_townsend_5 <- temp %>% arrange(IID)
+
+# Z = X - X_hat
+pgs_df_townsend_5$Z <- pgs_df_townsend_5$X - pgs_df_townsend_5$X_hat
+
+# Z ~ PGS
+Z_mod <- pgs_df_townsend_5 %>%
+  group_by(phenotype) %>%
+  do(lm_Z = lm(Z ~ pgs, data = .))
+
+# Apply this model
+temp <- pgs_df_townsend_5 %>% filter(phenotype == pheno[1])
+temp$Z_hat <- unname(predict(Z_mod$lm_Z[Z_mod$phenotype == pheno[1]][[1]], newdata = temp))
+for(trait in pheno[2:15]){
+  temp2 <- pgs_df_townsend_5 %>% filter(phenotype == trait)
+  temp2$Z_hat <- unname(predict(Z_mod$lm_Z[Z_mod$phenotype == trait][[1]], newdata = temp2))
+  temp <- rbind.data.frame(temp, temp2)
+}
+pgs_df_townsend_5 <- temp %>% arrange(IID)
+
+# Prediction error = (Z - Z_hat) ^ 2
+pgs_df_townsend_5$pred_error = (pgs_df_townsend_5$Z - pgs_df_townsend_5$Z_hat) ^ 2
+
+pgs_df_townsend_5 <- pgs_df_townsend_5 %>% arrange(IID, phenotype)
+
+pgs_df_townsend_5 %>% write_tsv("data/pgs_pred/ind_pgs_df_townsend_5.tsv")
+
+
+# GWAS w/ 300K WB
+group_non_pgs_df_300K <- read_tsv("data/pgs_pred/group_non_pgs_df_300K.tsv")
+
+# Read in PGS for threshold 1 (p < 1e-5)
+pgs_df_300K <- data.frame()
+for(trait in pheno){
+  temp <- read_tsv(paste0("data/pgs/", trait, "_300K_1_scores.sscore"))
+  temp$pgs <- temp$SCORE1_AVG
+  temp <- temp %>% select(-c(ALLELE_CT, NAMED_ALLELE_DOSAGE_SUM, SCORE1_AVG))
+  temp$phenotype <- rep(trait, nrow(temp))
+  pgs_df_300K <- rbind.data.frame(pgs_df_300K, temp)
+  rm(temp)
+}
+
+pgs_df_300K <- left_join(group_non_pgs_df_300K, pgs_df_300K, by = c("#FID", "IID", "phenotype"))
+pgs_df_300K <- pgs_df_300K %>% select(`#FID`, IID, sex, age, age_sq, age_sex, age_sq_sex, array_type,
+                            pc_dist, pgs, phenotype, phenotype_value, weighted_pc_groups, group_close_to_gwas)
+
+pgs_df_300K <- pgs_df_300K %>% mutate(
+  sex_pc_dist = sex * pc_dist,
+  pc_dist_sq = pc_dist ^ 2,
+  pc_dist_3 = pc_dist ^ 3,
+  pc_dist_4 = pc_dist ^ 4,
+  pc_dist_5 = pc_dist ^ 5,
+  pc_dist_6 = pc_dist ^ 6,
+  pc_dist_7 = pc_dist ^ 7,
+  pc_dist_8 = pc_dist ^ 8,
+  pc_dist_9 = pc_dist ^ 9,
+  pc_dist_10 = pc_dist ^ 10,
+  pc_dist_11 = pc_dist ^ 11,
+  pc_dist_12 = pc_dist ^ 12,
+  pc_dist_13 = pc_dist ^ 13,
+  pc_dist_14 = pc_dist ^ 14,
+  pc_dist_15 = pc_dist ^ 15,
+  pc_dist_16 = pc_dist ^ 16,
+  pc_dist_17 = pc_dist ^ 17,
+  pc_dist_18 = pc_dist ^ 18,
+  pc_dist_19 = pc_dist ^ 19,
+  pc_dist_20 = pc_dist ^ 20
+)
+
+# Fit a linear model of Y ~ covariates in each bin independently, 
+# startng with the first bin
+temp <- pgs_df_300K %>% filter(phenotype == pheno[1] & weighted_pc_groups == 1)
+# Only use array_type if both array types have more than 1 sample in this bin
+# (or lm doesn't run)
+if(length(temp$array_type[temp$array_type == "Axiom"]) > 1 &
+   length(temp$array_type[temp$array_type == "BiLEVE"]) > 1){
+  Y_mod <- temp %>%
+    lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex + array_type, data = .)
+  
+  # Apply the model onto this weighted_pc_groups
+  temp$Y_hat <- unname(predict(Y_mod, newdata = temp))
+  
+} else{
+  Y_mod <- temp %>%
+    lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex, data = .)
+  
+  # Apply the model onto this weighted_pc_groups
+  temp$Y_hat <- unname(predict(Y_mod, newdata = temp))
+}
+
+# Repeat for the other bins
+for(i in 2:250){
+  temp2 <- pgs_df_300K %>% filter(phenotype == pheno[1] & weighted_pc_groups == i)
+  if(length(temp2$array_type[temp2$array_type == "Axiom"]) > 1 &
+     length(temp2$array_type[temp2$array_type == "BiLEVE"]) > 1){
+    Y_mod <- temp2 %>%
+      lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex + array_type, data = .)
+    
+    temp2$Y_hat = unname(predict(Y_mod, newdata = temp2))
+    
+  } else{
+    Y_mod <- temp2 %>%
+      lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex, data = .)
+    
+    temp2$Y_hat <- unname(predict(Y_mod, newdata = temp2))
+  }
+  temp <- rbind.data.frame(temp, temp2)
+}
+
+# Do the same for the other phenotypes
+for(trait in pheno[2:15]){
+  print(trait)
+  for(i in 1:250){
+    temp2 <- pgs_df_300K %>% filter(phenotype == trait & weighted_pc_groups == i)
+    if(length(temp2$array_type[temp2$array_type == "Axiom"]) > 1 &
+       length(temp2$array_type[temp2$array_type == "BiLEVE"]) > 1){
+      Y_mod <- temp2 %>%
+        lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex + array_type, data = .)
+      
+      temp2$Y_hat = unname(predict(Y_mod, newdata = temp2))
+      
+    } else{
+      Y_mod <- temp2 %>%
+        lm(phenotype_value ~ sex + age + age_sq + age_sex + age_sq_sex, data = .)
+      
+      temp2$Y_hat <- unname(predict(Y_mod, newdata = temp2))
+    }
+    temp <- rbind.data.frame(temp, temp2)
+  }
+}
+
+pgs_df_300K <- temp %>% arrange(IID)
+
+# X = Y - Y_hat
+pgs_df_300K$X <- pgs_df_300K$phenotype_value - pgs_df_300K$Y_hat
+
+# Do X ~ gen_dist_polynomial + sex + sex_gen_dist across all bins
+X_mod <- pgs_df_300K %>%
+  group_by(phenotype) %>%
+  do(lm_X = lm(X ~ pc_dist + pc_dist_sq + pc_dist_3 + pc_dist_4 + pc_dist_5 + pc_dist_6 +
+                 pc_dist_7 + pc_dist_8 + pc_dist_9 + pc_dist_10 + pc_dist_11 + pc_dist_12 +
+                 pc_dist_13 + pc_dist_14 + pc_dist_15 + pc_dist_16 + pc_dist_17 + pc_dist_18 +
+                 pc_dist_19 + pc_dist_20 + sex + sex_pc_dist, data = .))
+
+# Apply this model
+temp <- pgs_df_300K %>% filter(phenotype == pheno[1])
+temp$X_hat <- unname(predict(X_mod$lm_X[X_mod$phenotype == pheno[1]][[1]], newdata = temp))
+for(trait in pheno[2:15]){
+  temp2 <- pgs_df_300K %>% filter(phenotype == trait)
+  temp2$X_hat <- unname(predict(X_mod$lm_X[X_mod$phenotype == trait][[1]], newdata = temp2))
+  temp <- rbind.data.frame(temp, temp2)
+}
+pgs_df_300K <- temp %>% arrange(IID)
+
+# Z = X - X_hat
+pgs_df_300K$Z <- pgs_df_300K$X - pgs_df_300K$X_hat
+
+# Z ~ PGS
+Z_mod <- pgs_df_300K %>%
+  group_by(phenotype) %>%
+  do(lm_Z = lm(Z ~ pgs, data = .))
+
+# Apply this model
+temp <- pgs_df_300K %>% filter(phenotype == pheno[1])
+temp$Z_hat <- unname(predict(Z_mod$lm_Z[Z_mod$phenotype == pheno[1]][[1]], newdata = temp))
+for(trait in pheno[2:15]){
+  temp2 <- pgs_df_300K %>% filter(phenotype == trait)
+  temp2$Z_hat <- unname(predict(Z_mod$lm_Z[Z_mod$phenotype == trait][[1]], newdata = temp2))
+  temp <- rbind.data.frame(temp, temp2)
+}
+pgs_df_300K <- temp %>% arrange(IID)
+
+# Prediction error = (Z - Z_hat) ^ 2
+pgs_df_300K$pred_error = (pgs_df_300K$Z - pgs_df_300K$Z_hat) ^ 2
+
+pgs_df_300K <- pgs_df_300K %>% arrange(IID, phenotype)
+
+pgs_df_300K %>% write_tsv("data/pgs_pred/ind_pgs_df_300K.tsv")
